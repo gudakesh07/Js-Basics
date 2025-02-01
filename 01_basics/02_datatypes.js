651220c1f99bf316e76dbcf692311fbf88a20071
RITU RAJ <riturajgudakesh@gmail.com>
2025-02-01 08:04:42 +0000

    Done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use Strict"; // treat all JS code as newer version.
+
+// alert("hello") // we are using nodejs, not browser.
+
+console.log(3
+    +
+    3
+) // code readability should be high.
+
+let name = "Gudakesh"; // String
+
+let age = 18 // Number
+
+let isLoggedIn = false // boolean
+
+// Number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null=> standalone value
+// undefined => value is not defined.
+// Symbol => unique.
+
+// All these datatypes form 16-22 are known as Primitive Data types.
+
+// Object
+
+console.log(typeof(name));
+console.log(typeof(undefined)); // undefined is a type of datatype.
+console.log(typeof(null)); // null is a type of datatype.
+
