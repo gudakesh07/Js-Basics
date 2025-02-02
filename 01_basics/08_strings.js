f749251a2216bf9316b237ea53aab58779f15866
RITU RAJ <riturajgudakesh@gmail.com>
2025-02-02 18:55:32 +0000

    do it again

---
+++ 01_basics/08_strings.js
@@ -0,0 +1,15 @@
+const name = "Gudakesh"
+const repoCount = 5
+
+// console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String("Gudakesh")
+
+console.log(gameName)
+
+//  Now remember that in python there was this concept of indexes here it it the same.. for that just use the square brackets
+
+console.log(name[0]); 
+
