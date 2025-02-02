a0eece1e23ecad2713ee0ef8562d2a77be525925
RITU RAJ <riturajgudakesh@gmail.com>
2025-02-02 13:34:29 +0000

    stack and heaps

---
+++ 01_basics/07_stack_heap.js
@@ -0,0 +1,20 @@
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "0xGudakesh07"
+let anotherName = myYoutubename
+anothername = "Gudakesh"
+
+console.log(myYoutubename)
+console.log(anotherName)
+
+let userOne = {
+    email : "user@google.com",
+    upi : "user@axis"
+}
+
+let userTwo = userOne
+
+userTwo.email = "gg@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
